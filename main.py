cbc48dbb8576582822f09428cbbe0577a79ba833
prakashkmr48 <prakashkmr48@gmail.com>
2023-09-21 19:40:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,49 +8,53 @@ api_key = '4d28967db8624f0eb0eca67fbf492984'
 base_url = 'https://newsapi.org/v2/top-headlines?'
 
 # Define the parameters for your news query
-country = 'US'   # You can change the country code
-page_size = 5    # Number of articles per page
 parameters = {
-    'country': country,
+    'country': 'US',   # You can change the country code
     'apiKey': api_key,
-    'pageSize': page_size
+    'pageSize': 10  # Adjust the number of articles to fetch as needed
 }
 
-# Function to fetch news articles
-def fetch_news(page):
-    parameters['page'] = page
-    response = requests.get(base_url, params=parameters)
-    if response.status_code == 200:
-        return response.json().get('articles', [])
-    else:
-        st.write('Failed to retrieve news data. Status code:', response.status_code)
-        return []
+# Function to fetch all news articles
+def fetch_all_news():
+    all_articles = []
+    current_page = 1
+    while True:
+        parameters['page'] = current_page
+        response = requests.get(base_url, params=parameters)
+        if response.status_code == 200:
+            news_data = response.json()
+            articles = news_data.get('articles', [])
+            if not articles:
+                break
+            all_articles.extend(articles)
+            current_page += 1
+        else:
+            st.write('Failed to retrieve news data. Status code:', response.status_code)
+            break
+    return all_articles
 
-# Initialize variables
-current_page = 1
-articles = fetch_news(current_page)
+# Fetch all news articles and store them
+all_news = fetch_all_news()
 current_article_index = 0
 
 # Display the app title
 st.title("News Headlines")
 
 # Check if articles are present
-if articles:
+if all_news:
     # Create buttons for navigation
     next_button = st.button("Next")
     prev_button = st.button("Previous")
 
     if next_button:
-        current_article_index = 0  # Reset to the first article on a new page
-        current_page += 1
-        articles = fetch_news(current_page)
+        # Show the next article
+        current_article_index = (current_article_index + 1) % len(all_news)
 
     if prev_button:
-        current_article_index = 0  # Reset to the first article on a new page
-        current_page = max(1, current_page - 1)
-        articles = fetch_news(current_page)
+        # Show the previous article
+        current_article_index = (current_article_index - 1) % len(all_news)
 
     # Display the current article's title
-    st.write(articles[current_article_index]['title'])
+    st.write(all_news[current_article_index]['title'])
 else:
     st.write('No articles found.')
