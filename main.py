a0a72ccbaba9731a8e9d6c3fbd551fe7a175d9f0
prakashkmr48 <prakashkmr48@gmail.com>
2023-09-21 20:52:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -29,8 +29,38 @@ def fetch_headlines():
 # Fetch and store headlines
 all_headlines = fetch_headlines()
 
+# Define custom CSS for styling headlines
+custom_css = """
+<style>
+.news-headline {
+    font-size: 24px;
+    font-weight: bold;
+    margin-bottom: 10px;
+    padding: 10px;
+    background-color: #f0f0f0;
+    border-radius: 5px;
+    box-shadow: 0 0 5px rgba(0, 0, 0, 0.2);
+    animation: fadeInUp 0.5s ease-in-out;
+}
+
+@keyframes fadeInUp {
+    0% {
+        opacity: 0;
+        transform: translateY(20px);
+    }
+    100% {
+        opacity: 1;
+        transform: translateY(0);
+    }
+}
+</style>
+"""
+
 # Display the app title
 st.title("News Headlines")
+
+# Inject custom CSS
+st.markdown(custom_css, unsafe_allow_html=True)
 
 # Check if headlines are present
 if all_headlines:
@@ -38,16 +68,18 @@ if all_headlines:
     headline_placeholder = st.empty()
 
     # Create a checkbox to pause and resume news
-    pause_news = st.checkbox("Pause/Resume News")
+    pause_news = st.checkbox("Pause News")
 
     # Initialize the current headline index
     current_headline_index = 0
 
     # Automatically update headlines in a continuous loop
     while True:
-        # Display the current headline if the "Pause/Resume News" checkbox is not selected
+        # Display the current headline if the "Pause News" checkbox is not selected
         if not pause_news:
-            headline_placeholder.write(all_headlines[current_headline_index])
+            # Apply custom CSS to the headline
+            headline_html = f'<div class="news-headline">{all_headlines[current_headline_index]}</div>'
+            headline_placeholder.markdown(headline_html, unsafe_allow_html=True)
 
             # Update the index for the next headline
             current_headline_index = (current_headline_index + 1) % len(all_headlines)
