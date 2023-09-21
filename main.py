f75dc1b0dc1141bb3e6b4192edc78c36fab47943
prakashkmr48 <prakashkmr48@gmail.com>
2023-09-21 19:37:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,43 +10,41 @@ base_url = 'https://newsapi.org/v2/top-headlines?'
 # Define the parameters for your news query
 parameters = {
     'country': 'US',   # You can change the country code
-    'apiKey': api_key
+    'apiKey': api_key,
+    'pageSize': 10  # Adjust the number of articles per page as needed
 }
 
-# Make a GET request to the News API
-response = requests.get(base_url, params=parameters)
-
-# Check if the request was successful
-if response.status_code == 200:
-    # Parse the JSON response
-    news_data = response.json()
-
-    # Display the app title
-    st.title("News Headlines")
-
-    # Check if articles are present in the response
-    if 'articles' in news_data:
-        # Store the list of articles
-        articles = news_data['articles']
-        
-        # Initialize an index to keep track of the current article
-        current_article_index = 0
-
-        # Create buttons for navigation
-        next_button = st.button("Next")
-        prev_button = st.button("Previous")
-
-        if next_button:
-            # Show the next article
-            current_article_index = (current_article_index + 1) % len(articles)
-
-        if prev_button:
-            # Show the previous article
-            current_article_index = (current_article_index - 1) % len(articles)
-
-        # Display the current article's title
-        st.write(articles[current_article_index]['title'])
+# Function to fetch news articles
+def fetch_news():
+    response = requests.get(base_url, params=parameters)
+    if response.status_code == 200:
+        return response.json().get('articles', [])
     else:
-        st.write('No articles found in the response.')
+        st.write('Failed to retrieve news data. Status code:', response.status_code)
+        return []
+
+# Initialize the list of articles
+articles = fetch_news()
+current_article_index = 0
+
+# Display the app title
+st.title("News Headlines")
+
+# Check if articles are present
+if articles:
+    # Create buttons for navigation
+    next_button = st.button("Next")
+    prev_button = st.button("Previous")
+
+    if next_button:
+        # Show the next article
+        current_article_index = (current_article_index + 1) % len(articles)
+
+    if prev_button:
+        # Show the previous article
+        current_article_index = (current_article_index - 1) % len(articles)
+
+    # Display the current article's title
+    st.write(articles[current_article_index]['title'])
 else:
-    st.write('Failed to retrieve news data. Status code:', response.status_code)
+    st.write('No articles found.')
