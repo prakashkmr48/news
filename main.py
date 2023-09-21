365013c95976f4ab6d2e5f9676c609def7b1f6de
prakashkmr48 <prakashkmr48@gmail.com>
2023-09-21 20:42:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -37,8 +37,9 @@ if all_headlines:
     # Create a placeholder for displaying headlines
     headline_placeholder = st.empty()
 
-    # Create a boolean variable to control the loop
+    # Create boolean variables to control the loop and pause
     run_headlines = True
+    pause_headlines = False
 
     # Initialize the current headline index
     current_headline_index = 0
@@ -46,31 +47,28 @@ if all_headlines:
     # Create a "Stop" button
     stop_button = st.button("Stop")
 
-    # Create a "Display News" button
-    display_button = st.button("Display News")
+    # Create a "Resume News" button
+    resume_button = st.button("Resume News")
 
     # Automatically update headlines in a continuous loop
     while run_headlines:
-        # Check if the "Stop" button is clicked
-        if stop_button:
-            run_headlines = False  # Stop the loop when the button is clicked
-            break
+        # Display the current headline
+        headline_placeholder.write(all_headlines[current_headline_index])
 
-        # Check if the "Display News" button is clicked
-        if display_button:
-            # Display the current headline
-            headline_placeholder.write(all_headlines[current_headline_index])
-
-            # Update the index for the next headline
-            current_headline_index = (current_headline_index + 1) % len(all_headlines)
-
-            # Reset the "Display News" button state
-            display_button = st.button("Display News")
+        # Update the index for the next headline
+        current_headline_index = (current_headline_index + 1) % len(all_headlines)
 
         # Sleep for 3 seconds before displaying the next headline
         time.sleep(3)
 
-    # Display a message when the loop is stopped
-    st.write("News Paused.")
+        # Check if the "Stop" button is clicked
+        if stop_button:
+            run_headlines = False  # Stop the loop when the button is clicked
+            headline_placeholder.empty()  # Clear the headline placeholder
+
+        # Check if the "Resume News" button is clicked
+        if resume_button:
+            run_headlines = True
+            resume_button = st.button("Resume News")  # Reset the button text
 else:
     st.write('No headlines found.')
